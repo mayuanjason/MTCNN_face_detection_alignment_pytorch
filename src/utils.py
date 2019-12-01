c095211108e9376cfdb29232b0f6750e62783c06
mayuanjason <mayuan_1999@outlook.com>
2019-12-01 21:36:29 +0800

    first version

---
+++ src/utils.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+from IPython import display
+import matplotlib.pyplot as plt
+import torch
+
+
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg')
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set matplotlib figure size."""
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+
+def try_gpu():
+    use_cuda = torch.cuda.is_available()
+    return torch.device("cuda" if use_cuda else "cpu")
+
+
+def bbox_to_rect(bbox, color):
+    # 将边界框(左上x, 左上y, 右下x, 右下y)格式转换成matplotlib格式：
+    # ((左上x, 左上y), 宽, 高)
+    return plt.Rectangle(
+        xy=(bbox[0], bbox[1]), width=bbox[2] - bbox[0], height=bbox[3] - bbox[1],
+        fill=False, edgecolor=color, linewidth=2)
+
+
+def show_bboxes(axes, bboxes, labels=None, colors=None):
+    def _make_list(obj, default_values=None):
+        if obj is None:
+            obj = default_values
+        elif not isinstance(obj, (list, tuple)):
+            obj = [obj]
+        return obj
+    
+    labels = _make_list(labels)
+    colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
+    for i, box in enumerate(bboxes):
+        color = colors[i % len(colors)]
+        rect = bbox_to_rect(box, color)
+        axes.add_patch(rect)
+        if labels and len(labels) > 1:
+            text_color = 'k' if color == 'w' else 'w'
+            axes.text(rect.xy[0], rect.xy[1], labels[i],
+                      va='center', ha='center', fontsize=9, color=text_color,
+                      bbox=dict(facecolor=color, lw=0))
